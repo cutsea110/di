b0eb44e96569bf04bbd9c830d0633108014d1f09
cutsea110 <cutsea110@gmail.com>
2019-07-21 06:35:46 +0900

    tagless final

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ref.) https://qiita.com/lotz/items/a903d3b2aec0c1d4f3ce
 DI implemented by using Extensible Effects and Tagless Final.
+
+compile
+----------
+$ ghc-prep -package-db .cabal-sandbox/x86_64-linux-ghc-8.4.4-packages.conf.d/ di.hs
+
--- di.hs
+++ di.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Main where
 
 import Prelude hiding (read)
+import Control.Monad.Reader
+import qualified Database.Redis as Redis
 import qualified Data.ByteString as BS
 import Data.IORef
 
@@ -9,24 +13,34 @@ type Key   = BS.ByteString
 type Value = BS.ByteString
 
 -- | interface for simple KVS.
-class DataStore a where
-  create :: Key -> Value -> a -> IO ()
-  read   :: Key          -> a -> IO (Maybe Value)
-  update :: Key -> Value -> a -> IO ()
-  delete :: Key          -> a -> IO ()
+class Monad repr => DataStoreSYM repr where
+  create :: Key -> Value -> repr ()
+  read   :: Key          -> repr (Maybe Value)
+  update :: Key -> Value -> repr ()
+  delete :: Key          -> repr ()
 
 -- | update when the Key already exists, the other case insert it.
-upsert :: DataStore a => Key -> Value -> a -> IO ()
-upsert k v ds = do
-  peek <- read k ds
+upsert :: DataStoreSYM repr => Key -> Value -> repr ()
+upsert k v = do
+  peek <- read k
   case peek of
-    Just _  -> update k v ds
-    Nothing -> create k v ds
+    Just _  -> update k v
+    Nothing -> create k v
 
 -------------------------------
 
+-- | implementation for Mock which has no action
+instance DataStoreSYM IO where
+  create k v = BS.putStrLn $ BS.concat ["Create: ", k, ", ", v]
+  read   k   = Nothing <$ (BS.putStrLn $ BS.concat ["Read: ", k])
+  update k v = BS.putStrLn $ BS.concat ["Update: ", k, ", ", v]
+  delete k   = BS.putStrLn $ BS.concat ["Delete: ", k]
+
+runMockDS ::  IO a -> IO a
+runMockDS = id
+
 -- | simple implementation of KVS by using IORef
-newtype IORefImpl = IORefImpl (IORef [(Key, Value)])
+type IORefDS = ReaderT (IORef [(Key, Value)]) IO
 
 updateList :: Eq k => k -> v -> [(k, v)] -> [(k, v)]
 updateList k v = map (\r@(k', _) -> if k' == k then (k, v) else r)
@@ -34,31 +48,41 @@ updateList k v = map (\r@(k', _) -> if k' == k then (k, v) else r)
 deleteList :: Eq k => k -> [(k, v)] -> [(k, v)]
 deleteList k = filter (\(k', _) -> k' /= k)
 
-instance DataStore IORefImpl where
-  create k v (IORefImpl ref) = modifyIORef ref ((k, v):)
-  read   k   (IORefImpl ref) = lookup k <$> readIORef ref
-  update k v (IORefImpl ref) = modifyIORef ref (updateList k v)
-  delete k   (IORefImpl ref) = modifyIORef ref (deleteList k)
+instance DataStoreSYM IORefDS where
+  create k v = ask >>= (\ref -> liftIO $ modifyIORef ref ((k, v):))
+  read   k   = ask >>= (\ref -> liftIO $ lookup k <$> readIORef ref)
+  update k v = ask >>= (\ref -> liftIO $ modifyIORef ref (updateList k v))
+  delete k   = ask >>= (\ref -> liftIO $ modifyIORef ref (deleteList k))
 
--- | implementation for Mock which has no action
-data MockImpl = MockImpl
+runIORefDS :: IORef [(Key, Value)] -> IORefDS a -> IO a
+runIORefDS ref dsl = runReaderT dsl ref
 
-instance DataStore MockImpl where
-  create k v _ = BS.putStrLn $ BS.concat ["Create: ", k, ", ", v]
-  read   k   _ = Nothing <$ (BS.putStrLn $ BS.concat ["Read: ", k])
-  update k v _ = BS.putStrLn $ BS.concat ["Update: ", k, ", ", v]
-  delete k   _ = BS.putStrLn $ BS.concat ["Delete: ", k]
+handleError :: Show a => Either a b -> b
+handleError = either (error . show) id
 
-testMockImpl = upsert "key" "value" MockImpl
-testIORefImpl = do
+instance DataStoreSYM Redis.Redis where
+  create k v = () <$ Redis.set k v
+  read   k   = handleError <$> Redis.get k
+  update k v = () <$ Redis.set k v
+  delete k   = () <$ Redis.del [k]
+
+runRedis :: Redis.Connection -> Redis.Redis a -> IO a
+runRedis conn redis = Redis.runRedis conn redis
+
+testMockDS :: IO ()
+testMockDS = runMockDS $ upsert "key" "value"
+
+testIORefDS :: IO ()
+testIORefDS = do
   ref <- newIORef ([] :: [(Key, Value)])
-  upsert "key" "value" (IORefImpl ref)
-  kvs <- readIORef ref
-  print kvs
+  runIORefDS ref $ upsert "key" "value"
+  v <- readIORef ref
+  print v
+  return ()
 
 main :: IO ()
 main = do
-  print "testing MockImpl ... "
-  testMockImpl
-  print "testing IORefImpl ... "
-  testIORefImpl
+  print "testing mockDS"
+  testMockDS
+  print "testing mockIORefDS"
+  testIORefDS
